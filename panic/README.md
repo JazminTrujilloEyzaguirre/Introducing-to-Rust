a8bab3f8aadc47fe71c8468387ca4f9dcc83c678
jazminTrujillo <jazmin.jahazieltru@gmail.com>
2022-03-14 15:38:46 -0300

    (feat): add panic

---
+++ panic/README.md
@@ -0,0 +1,2 @@
+# This is part of my personal notes in Notion
+More Info: https://parsiomnium.notion.site/Panic-582e8341668d47aa9dabcc652d3854eb
---
+++ panic/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hola, soy un mensaje de la línea 3");
+    println!("Hola, soy un mensaje de la línea 5");
+    panic!("El programa finaliza de forma inesperda!!!!");
+    println!("Hola, soy un mensaje de la línea 9");
+    println!("Hola, soy un mensaje de la línea 9");
+}
